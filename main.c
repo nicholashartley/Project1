aa6b47795a4685d83f7cfeffd12aa53a7e5d94f7
Nicholas Hartley <nicholas.hartley@uon.edu.au>
2019-04-04 04:28:01 +0000

    1527

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    
-int a, b, c;
-scanf("%d %d %d", &a, &b, &c);
-printf("found integers  %d %d %d\n", a, b, c);
 
-return 0;
+
+int main() {
+  
+   
+printf("Please select a menu option: \n");
+printf("a) Encrypt a rotation cipher: \n");
+printf("b) Decrypt a rotation cipher: \n");
+printf("c) Encrypt a substitution cipher: \n");
+printf("d) Decrypt a substitution cipher: \n");
+
+scanf("%s", cipher);
+
 
 
 }
